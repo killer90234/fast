681bef0f9cee8f5cd014baa9bd2dc123bd8473c8
killer90234 <157220131+killer90234@users.noreply.github.com>
2025-09-21 22:34:36 +0530

    Update db.py

--- db.py
+++ db.py
@@ -7,4 +7,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+base = declarative_base()
